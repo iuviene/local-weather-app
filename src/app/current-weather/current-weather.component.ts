778c49035a4eb43e9fa1c6bf91ffb8e4765fa5ec
Nick Iuviene <iuviene@gmail.com>
2018-10-31 22:07:12 -0400

    completed the first feature!

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,20 @@
+import { WeatherService } from './../weather/weather.service';
+import { ICurrentWeather } from './../interfaces';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather;
+  constructor(private weatherService: WeatherService) {
+  }
+
+  ngOnInit() {
+    this.weatherService.getCurrentWeather('New York', 'US')
+      .subscribe((data) => this.current = data);
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: '53ae7a2253e8a2ca3897e706a0919d78',
+  baseUrl: 'http://',
+
 };
 
 /*
